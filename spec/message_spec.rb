9cb5332d5d26d27224a03e377e42068812052eda
Muhammad Ridzwan Dinesh <maslya0309@gmail.com>
2022-04-11 05:58:35 +0800

    Initialize ridzwan6919-patch-1

---
+++ spec/message_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe "A Gmail message" do
+  context "on initialize" do
+    it "should set uid and mailbox" do
+      skip
+    end
+  end
+  
+  context "instance" do
+    it "should be able to mark itself as read" do
+      skip
+    end
+    
+    it "should be able to mark itself as unread" do
+      skip
+    end
+    
+    it "should be able to set star itself" do
+      skip
+    end
+    
+    it "should be able to unset start" do
+      skip
+    end
+    
+    it "should be able to archive itself" do
+      skip
+    end
+    
+    it "should be able to delete itself" do
+      skip
+    end
+    
+    it "should be able to move itself to spam" do
+      skip
+    end
+    
+    it "should be able to set given label" do
+      skip
+    end
+    
+    it "should be able to mark itself with given flag" do
+      skip
+    end
+    
+    it "should be able to move itself to given box" do
+      skip
+    end
+  end 
+end
